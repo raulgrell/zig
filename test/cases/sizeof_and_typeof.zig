6c0e27286f5f11e541e46a79b15d25b5d9b9845d
Raul Leal <raulgrell@gmail.com>
2017-04-19 14:37:02 +0000

    Add builtin function @offsetOf

--- test/cases/sizeof_and_typeof.zig
+++ test/cases/sizeof_and_typeof.zig
@@ -6,3 +6,31 @@ test "sizeofAndTypeOf" {
 }
 const x: u16 = 13;
 const z: @typeOf(x) = 19;
+
+const A = struct {
+    a: u8,
+    b: u32,
+    c: u8,
+};
+
+const P = packed struct {
+    a: u8,
+    b: u32,
+    c: u8,
+};
+
+test "offsetOf" {
+    const a: A = undefined;
+    const p: P = undefined;
+
+    const aa = @offsetOf(@typeOf(a), "a");
+    const ab = @offsetOf(@typeOf(a), "b");
+    const ac = @offsetOf(@typeOf(a), "c");
+
+    const pa = @offsetOf(@typeOf(p), "a");
+    const pb = @offsetOf(@typeOf(p), "b");
+    const pc = @offsetOf(@typeOf(p), "c");
+
+    assert(aa == 0 and ab == 4 and ac == 8);
+    assert(pa == 0 and pb == 1 and pc == 5);
+}
